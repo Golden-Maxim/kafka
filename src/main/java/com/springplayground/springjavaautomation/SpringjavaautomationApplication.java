c5bc2429dfb9c4629b2bcb3fc9f4698e54b2ab41
maksym.stovbchatyi <maksym.stovbchatyi@avenga.com>
2024-07-26 15:35:31 +0300

    init

---
+++ src/main/java/com/springplayground/springjavaautomation/SpringjavaautomationApplication.java
@@ -0,0 +1,13 @@
+package com.springplayground.springjavaautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjavaautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjavaautomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springplayground/springjavaautomation/SpringjavaautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springplayground.springjavaautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavaautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
